bb31f2b52369f5a0a16a7874d958a11e43534b97
teasec4 <85108614+teasec4@users.noreply.github.com>
2022-02-11 19:06:36 +0800

    Add files via upload

--- DjangoGym/settings.py
+++ DjangoGym/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = 'static/'
+STATIC_ROOT = 'todolist/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
