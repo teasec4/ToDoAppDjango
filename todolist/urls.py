9be61093898a077df339fd7b97769ba1e1810beb
teasec4 <teasec4@yandex.ru>
2022-02-14 12:56:43 +0800

    remove merge

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -7,4 +7,7 @@ urlpatterns = [
     path('mark_complete/<int:todo_id>', views.mark_complete, name="mark_complete"),
     path('mark_incomplete/<int:todo_id>', views.mark_incomplete, name="mark_incomplete"),
     path('edit/<int:todo_id>', views.edit, name="edit"),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
